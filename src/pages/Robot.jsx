b4e910b589554463af6f178c5fc59fcb4813af09
BorisH <87735930+bnhdez@users.noreply.github.com>
2026-01-11 20:32:29 -0600

    img robot not showing

--- src/pages/Robot.jsx
+++ src/pages/Robot.jsx
@@ -11,7 +11,7 @@ const Robot = () => {
             <div className="max-80 mx-auto px-3">
                 <div className="row gap-4 gap-md-0 d-flex justify-content-around">
                     <div className="col-md-6 d-flex align-items-center justify-content-center">
-                        <img src="img/projects/robot-detail.png" className="img-fluid shadow" alt="Robot Detail" />
+                        <img src="img/projects/Rescue_Robot.png" className="img-fluid shadow" alt="Robot Detail" />
                     </div>
                     <div className="col-md-6 d-flex align-items-center justify-content-center">
                         <img src="img/projects/robot.png" className="img-fluid shadow" alt="Robot" />
